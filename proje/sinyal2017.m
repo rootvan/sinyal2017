49972a9240c0f34ccdf6dd5dc540067d58256194
Ridvan Iszu <ridvan.karadag@std.iszu.edu.tr>
2017-12-31 18:19:22 +0300

    Genlik Spektrumu grafiği çizdirildi

--- proje/sinyal2017.m
+++ proje/sinyal2017.m
@@ -3,7 +3,19 @@ figure
 plot(t, ecg_noise)
 title("Gurultulu Sinyal");
 
-
+%%Genlik Spektrumu Grafiði
+figure
+ts = (1 / fs ) ;
+x = ecg_noise;
+y = fft(x);
+n = length(x);          % number of samples
+f = (0:n-1)*(fs/n);
+power = abs(y).^2/n;    % power of the DFT
+y0 = fftshift(y);         % shift y values
+f0 = (-n/2:n/2-1)*(fs/n); % 0-centered frequency range
+power0 = abs(y0);    % 0-centered power
+plot(f0,power0)
+title("Genlik Spektrumu");
 
 
 
